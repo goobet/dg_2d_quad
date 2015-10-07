ba1c9c59b8d6f2314d5de4e382d41cb939ab3516
Dmitriy Galkin <dmga13@gmail.com>
2015-10-07 10:27:36 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ const int N = 100;
 
 const double H = 1.0/N;
 
-const double TMAX = 1.0;
+const double TMAX = 10.0;
 const double TAU = 1.e-2;
 
 const int FILE_SAVE_STEP = 10;
@@ -23,18 +23,18 @@ Point  c[N+2][N+2]; // центры ячеек
 VECTOR u1[N+2][N+2]; // сюда суммируем интегралы
 
 Point cellGP[4];
-double cellGW[4];
+double cellGW[4] = {1, 1, 1, 1};
 
 // матрица масс
 double			****matrA;
 double			****matrInvA;
 
-int step =0;
+int step = 0;
 double t = 0.0;
 
 const int FUNC_COUNT = 3;
 
-double sqrt3 = 1.0/sqrt(3.0);
+double sqrt3 = 1.0/3.0;
 
 VECTOR getF(int i, int j, Point pt);
 VECTOR getDFDX(int iCell, Point pt);
@@ -52,6 +52,8 @@ void output();
 void saveResult(char *fName);
 void fillWithInitialData();
 void calculateCellCenters();
+int gaus_obr(int cnt_str, double **mass, double **M_obr);
+void set_bounds();
 
 int main() {
     // заполняем вспомогательные массивы
@@ -59,9 +61,13 @@ int main() {
     fillGaussPoints();
     calculateMassMatrix();
     fillWithInitialData();
+    output();
 
     while (t < TMAX) {
         incrementTime();
+
+        // Граничные условия
+        set_bounds();
 
         // Правая часть уравнения
         calculateDoubleIntegral(); // вычисляем двойной интеграл
@@ -77,8 +83,7 @@ int main() {
     return 0;
 }
 
-void fillGaussPoints()
-{
+void fillGaussPoints() {
     cellGP[0].x = -sqrt3;
     cellGP[0].y = -sqrt3;
 
@@ -90,11 +95,11 @@ void fillGaussPoints()
 
     cellGP[3].x = -sqrt3;
     cellGP[3].y =  sqrt3;
+    sqrt3 = 1.0/sqrt(3.0);
     // todo: cellGW ??????
 }
 
-void calculateMassMatrix()
-{
+void calculateMassMatrix() {
     matrA = new double***[N+2];
     matrInvA = new double***[N+2];
     for (int i = 0; i < N+2; i++) {
@@ -120,10 +125,9 @@ void calculateMassMatrix()
                 }
             }
             for (int igp = 0; igp < 4; igp++) {
-                Point gp = cellGP[i];
-                gp *= H; // todo: неоходимо изменить если не квадратные ячейки
+                Point gp = cellGP[igp];
+                gp *= H/2; // todo: неоходимо изменить если не квадратные ячейки
                 gp += c[i][j];
-                gp *= 0.5;
                 VECTOR phi1 = getF(i,j, gp);
                 VECTOR phi2 = getF(i,j, gp);
                 for (int m = 0; m < FUNC_COUNT; m++) {
@@ -132,21 +136,21 @@ void calculateMassMatrix()
                     }
                 }
             }
+
+            gaus_obr(FUNC_COUNT, a, matrInvA[i][j]);
         }
     }
 }
 
-void calculateDoubleIntegral()
-{
+void calculateDoubleIntegral() {
     for (int i = 1; i < N+1; i++) {
         for (int j = 0; j < N+1; j++) {
             VECTOR s(FUNC_COUNT);
             s = 0.0;
             for (int igp = 0; igp < 4; igp++) {
-                Point gp = cellGP[i];
-                gp *= H; // todo: неоходимо изменить если не квадратные ячейки
+                Point gp = cellGP[igp];
+                gp *= H/2; // todo: неоходимо изменить если не квадратные ячейки
                 gp += c[i][j];
-                gp *= 0.5;
                 s += getU(i,j,gp)*(getDFDX(i,gp)+getDFDY(i,gp));
                 s *= cellGW[igp];
             }
@@ -156,8 +160,7 @@ void calculateDoubleIntegral()
     }
 }
 
-void lineIntegralXDirection()
-{
+void lineIntegralXDirection() {
     for (int i = 0; i < N+1; i++) {
         for (int j = 1; j < N+1; j++) {
             Point gp1, gp2;
@@ -192,8 +195,7 @@ void lineIntegralXDirection()
     }
 }
 
-void lineIntegralYDirection()
-{
+void lineIntegralYDirection() {
     for (int i = 1; i < N+1; i++) {
         for (int j = 0; j < N+1; j++) {
             Point gp1, gp2;
@@ -228,15 +230,13 @@ void lineIntegralYDirection()
     }
 }
 
-void calculateLineIntegral()
-{
+void calculateLineIntegral() {
     lineIntegralXDirection(); // x-direction
 
     lineIntegralYDirection(); // y-direction
 }
 
-void calculateSolution()
-{
+void calculateSolution() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             u1[i][j] *= matrInvA[i][j];
@@ -247,8 +247,7 @@ void calculateSolution()
     }
 }
 
-VECTOR getF(int i, int j, Point pt)
-{
+VECTOR getF(int i, int j, Point pt) {
     if (FUNC_COUNT == 3 )
     {
         VECTOR v(3);
@@ -262,8 +261,7 @@ VECTOR getF(int i, int j, Point pt)
     }
 }
 
-VECTOR getDFDX(int iCell, Point pt)
-{
+VECTOR getDFDX(int iCell, Point pt) {
     if (FUNC_COUNT == 3 )
     {
         VECTOR v(3);
@@ -277,8 +275,7 @@ VECTOR getDFDX(int iCell, Point pt)
     }
 }
 
-VECTOR getDFDY(int iCell, Point pt)
-{
+VECTOR getDFDY(int iCell, Point pt) {
     if (FUNC_COUNT == 3 )
     {
         VECTOR v(3);
@@ -292,8 +289,7 @@ VECTOR getDFDY(int iCell, Point pt)
     }
 }
 
-void output()
-{
+void output() {
     if (step % FILE_SAVE_STEP  == 0)
     {
         char fName[50];
@@ -308,8 +304,7 @@ void output()
     }
 }
 
-void saveResult(char *fName)
-{
+void saveResult(char *fName) {
     FILE * fp = fopen(fName, "w");
 
     fprintf(fp, "x,y,u\n");
@@ -325,18 +320,18 @@ void saveResult(char *fName)
     fclose(fp);
 }
 
-void incrementTime()
-{
+void incrementTime() {
     step++;
     t += TAU;
 }
 
-void fillWithInitialData()
-{
+void fillWithInitialData() {
     const double r = 0.2;
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {
+    for (int i = 0; i <N+1; i++) {
+        for (int j = 0; j < N+1; j++) {
             u[i][j] = VECTOR(3);
+            u1[i][j] = VECTOR(3);
+
             Point lc = c[i][j];
             if ((lc.x-0.5)*(lc.x-0.5) + (lc.y-0.5)*(lc.y-0.5) < r*r)
             {
@@ -351,5 +346,47 @@ void calculateCellCenters() {
         for (int j = 1; j < N; j++) {
             c[i][j] = Point(i*H + H/2, j*H + H/2);
         }
+    }
+}
+
+int gaus_obr(int cnt_str, double **mass, double **M_obr) {
+    int i, j, k;
+
+    for (i = 0; i < cnt_str; i++) {
+        for (j = 0; j < cnt_str; j++)
+            M_obr[i][j] = 0;
+        M_obr[i][i] = 1;
+    }
+
+    double a, b;
+    for (i = 0; i < cnt_str; i++) {
+        a = mass[i][i];
+        for (j = i + 1; j < cnt_str; j++) {
+            b = mass[j][i];
+            for (k = 0; k < cnt_str; k++) {
+                mass[j][k] = mass[i][k] * b - mass[j][k] * a;
+                M_obr[j][k] = M_obr[i][k] * b - M_obr[j][k] * a;
+            }
+        }
+    }
+
+    double sum;
+    for (i = 0; i < cnt_str; i++) {
+        for (j = cnt_str - 1; j >= 0; j--) {
+            sum = 0;
+            for (k = cnt_str - 1; k > j; k--)
+                sum += mass[j][k] * M_obr[k][i];
+            M_obr[j][i] = (M_obr[j][i] - sum) / mass[j][j];
+        }
+    }
+    return 1;
+}
+
+void set_bounds() {
+    for (int i = 1; i < N; i++) {
+        u[0][i] = u[N][i];
+        u[N+1][i] = u[1][i];
+        u[i][0] = u[i][N];
+        u[i][N+1] = u[i][0];
     }
 }
